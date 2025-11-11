3e93f57d60b2b897d10fea93b7b58c03d16f71fb
navodita0027 <77601280+navodita0027@users.noreply.github.com>
2025-11-11 17:17:11 +0530

    Add files via upload

---
+++ LibraryApplication.java
@@ -0,0 +1,15 @@
+package com.navodita.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+public class LibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
